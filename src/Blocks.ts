0f2a3ca0cb6e2801f740ec78318fc9cfb27e5e18
Renan GEHAN <rgehan94@gmail.com>
2018-10-16 13:29:26 +0200

    Change water color

--- src/Blocks.ts
+++ src/Blocks.ts
@@ -17,7 +17,7 @@ const BLOCKS_COLORS = {
   [Blocks.COPPER]: '#96613c',
   [Blocks.COAL]: '#333333',
   [Blocks.GRASS]: '#4b771f',
-  [Blocks.WATER]: 'blue',
+  [Blocks.WATER]: '#233e84',
 };
 
 export function getBlockColor(block: Blocks) {
